3e3ec71d0cc10e5a27f63bcef9677b85b12d3b09
Jesse Govart <govartjessy@gmail.com>
2021-04-01 22:51:01 +0000

    d

--- src/index.js
+++ src/index.js
@@ -4,24 +4,54 @@ class MyCounter extends HTMLElement {
   constructor() {
     super();
 
-    const template = document.createElement("template");
-    template.innerHTML = /*html*/`
-      <style>
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.style.display = 'block';
+    svg.setAttribute("viewBox", "0 0 100 10");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    this.track = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    this.track.setAttribute("x", "0");
+    this.track.setAttribute("y", "0");
+    this.track.setAttribute("width", '100');
+    this.track.setAttribute("height", "10");
+    this.indicator = this.track.cloneNode(true);
+    this.indicator.setAttribute("width", '0');
+    this.track.style.fill = "var(--track-color, #9E9E9E)";
+    this.indicator.style.fill = "var(--track-color, red)";
 
-      </style>
-      <p>${ this.getAttribute('value') }</p>
-      <svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
-        <!-- Simple rect element -->
-        <rect x="0" y="0" width="${ this.getAttribute('value') }" height="20" />
-      </svg>
-      `;
-   
-    this.attachShadow({ mode: "open" });
+    svg.appendChild(this.track);
+    svg.appendChild(this.indicator);
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.appendChild(svg);
+
+    const time = {
+      start: performance.now(),
+      total: 1200
+    };
+    const easeOut = progress => Math.pow(--progress, 5) + 1;
+    const easeOutElastic = progress => Math.pow(2, -10 * progress) * Math.sin((progress - .1) * 5 * Math.PI) + 1;
+
+    const finalPosition = this.getAttribute('value');    
+    const tick = now => {
+      time.elapsed = now - time.start;
+      const progress = time.elapsed / time.total;
+      const easing = easeOutElastic(progress);
+      const position = easing * finalPosition;
+      // element.style.transform = `translate(${position}px)`;
+      this.indicator.setAttribute("width", position);
+      if (progress < 1) requestAnimationFrame(tick);
+    };
+
+    requestAnimationFrame(tick);
   }
 
   connectedCallback() {
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    
+
+    
+
   }
+
+
 }
 
 customElements.define("bar-chart", MyCounter);
