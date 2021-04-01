3bde8ab11183bc04976b55f244f8aed0209dca90
Jesse Govart <govartjessy@gmail.com>
2021-04-01 17:31:37 +0000

    s

--- src/index.js
+++ src/index.js
@@ -1,55 +1,27 @@
-const template = document.createElement("template");
-template.innerHTML = /*html*/`
-  <style>
-    * {
-      font-size: 200%;
-    }
 
-    span {
-      width: 4rem;
-      display: inline-block;
-      text-align: center;
-    }
-
-    button {
-      width: 4rem;
-      height: 4rem;
-      border: none;
-      border-radius: 10px;
-      background-color: seagreen;
-      color: white;
-    }
-  </style>
-  test
-  <button id="dec">-</button>
-  <span id="count"></span>
-  <button id="inc">+</button>`;
 
 class MyCounter extends HTMLElement {
   constructor() {
     super();
-    this.count = 0;
+
+    const template = document.createElement("template");
+    template.innerHTML = /*html*/`
+      <style>
+
+      </style>
+      <p>${ this.getAttribute('value') }</p>
+      <svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
+        <!-- Simple rect element -->
+        <rect x="0" y="0" width="${ this.getAttribute('value') }" height="20" />
+      </svg>
+      `;
+   
     this.attachShadow({ mode: "open" });
   }
 
   connectedCallback() {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.getElementById("inc").onclick = () => this.inc();
-    this.shadowRoot.getElementById("dec").onclick = () => this.dec();
-    this.update(this.count);
-  }
-
-  inc() {
-    this.update(++this.count);
-  }
-
-  dec() {
-    this.update(--this.count);
-  }
-
-  update(count) {
-    this.shadowRoot.getElementById("count").innerHTML = count;
   }
 }
 
-customElements.define("my-counter", MyCounter);
+customElements.define("bar-chart", MyCounter);
